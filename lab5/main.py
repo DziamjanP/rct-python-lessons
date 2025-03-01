991ad42f9ac3855461e81f6228e9c1068325c233
DziamjanP <demyan.postnikov@gmail.com>
2025-03-01 16:44:55 +0300

    make db global add task implementation

--- lab5/main.py
+++ lab5/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Query
 
 from db import DBHandler
+from task import Task, TaskModel
 
 app = FastAPI()
+
+db = DBHandler()
 
 @app.get('/')
 def index():
@@ -11,16 +14,15 @@ def index():
 @app.get('/tasks')
 def get_tasks(id: int = Query(None), completed: bool = Query(None), limit: int = Query(None)):
     #get task(s) and filter them
-    db = DBHandler()
     tasks = db.get_tasks(id = id, completed = completed, limit = limit)
     return {'tasks':tasks}
 
-@app.put('/tasks')
-def add_task():
-    #add new task
+@app.post('/tasks')
+def add_task(task: TaskModel):
+    db.add_task(Task(title=task.title, completed = task.completed, description = task.description, deadline = task.deadline))
     return {'code':0}
 
-@app.post('/tasks')
+@app.put('/tasks')
 def update_task():
     #update task
     return {'code':0}
