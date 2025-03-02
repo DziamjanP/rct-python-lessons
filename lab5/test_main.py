ad7443450c9bb061405ec549032567ccbd7b39a5
DziamjanP <demyan.postnikov@gmail.com>
2025-03-02 14:39:18 +0300

    add task update and delete tests

--- lab5/test_main.py
+++ lab5/test_main.py
@@ -14,6 +14,9 @@ def test_task_add():
     global task_id
     task_id = int(r.json()["msg"])
 
+    r = client.get(f"/tasks?id={task_id}")
+    assert r.status_code == 200
+    assert r.json()["tasks"][0] == TaskModel(id=task_id, title="Test task", description="test").model_dump()
 
 def test_task_get_error():
     r = client.get(f"/tasks?id={task_id}&completed=True&limit=1")
@@ -24,3 +27,19 @@ def test_task_get_success():
     assert r.status_code == 200
     assert r.json()["tasks"][0] == TaskModel(id=task_id, title="Test task", description="test").model_dump()
     
+def test_task_update():
+    r = client.put(f"/tasks/{task_id}", json={"completed":True, "description":"new description"})
+    assert r.status_code == 200
+    assert r.json() == TaskActionResponse.make_success().model_dump()
+
+    r = client.get(f"/tasks?id={task_id}")
+    assert r.status_code == 200
+    assert r.json()["tasks"][0] == TaskModel(id=task_id, title="Test task", description="new description", completed=True).model_dump()
+
+def test_task_delete():
+    r = client.delete(f"/tasks/{task_id}")
+    assert r.status_code == 200
+    assert r.json() == TaskActionResponse.make_success().model_dump()
+
+    r = client.get(f"/tasks?id={task_id}")
+    assert r.status_code == 404
